c38471c9b2205ef16dc7e5d3962d2f5d57ccd215
mehmet-dogru <mdogru685@gmail.com>
2024-03-29 12:49:57 +0300

    refactor: implemented sql queries to user operations

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,10 +1,9 @@
 package repository
 
 import (
+	"database/sql"
 	"errors"
 	"go-blog-app/internal/domain"
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 	"log"
 )
 
@@ -16,60 +15,54 @@ type UserRepository interface {
 }
 
 type userRepository struct {
-	db *gorm.DB
+	db *sql.DB
 }
 
-func NewUserRepository(db *gorm.DB) UserRepository {
+func NewUserRepository(db *sql.DB) UserRepository {
 	return &userRepository{
 		db: db,
 	}
 }
 
 func (r userRepository) CreateUser(user domain.User) (domain.User, error) {
-	err := r.db.Create(&user).Error
+	query := `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)`
+	_, err := r.db.Exec(query, user.FirstName, user.LastName, user.Email, user.Password)
 	if err != nil {
 		log.Printf("create user error %v", err)
-		return domain.User{}, errors.New("failed to create user")
+		return domain.User{}, err
 	}
 	return user, nil
 }
 
 func (r userRepository) FindUser(email string) (domain.User, error) {
 	var user domain.User
-
-	err := r.db.First(&user, "email=?", email).Error
-
+	query := `SELECT id, first_name, last_name, email, role, password FROM users WHERE email = $1`
+	err := r.db.QueryRow(query, email).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Role, &user.Password)
 	if err != nil {
 		log.Printf("find user error %v", err)
 		return domain.User{}, errors.New("user does not exist")
 	}
-
 	return user, nil
 }
 
 func (r userRepository) FindUserById(id uint) (domain.User, error) {
 	var user domain.User
-
-	err := r.db.First(&user, id).Error
-
+	query := `SELECT id, first_name, last_name, email, role FROM users WHERE id = $1`
+	err := r.db.QueryRow(query, id).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Role)
 	if err != nil {
 		log.Printf("find user error %v", err)
 		return domain.User{}, errors.New("user does not exist")
 	}
-
 	return user, nil
 }
 
 func (r userRepository) UpdateUser(id uint, u domain.User) (domain.User, error) {
-
 	var user domain.User
-
-	err := r.db.Model(&user).Clauses(clause.Returning{}).Where("id=?", id).Updates(u).Error
-
+	query := `UPDATE users SET first_name = $1, last_name = $2, email = $3, password = $4 WHERE id = $5`
+	_, err := r.db.Exec(query, u.FirstName, u.LastName, u.Email, u.Password, id)
 	if err != nil {
 		log.Printf("error on update %v", err)
 		return domain.User{}, errors.New("failed update user")
 	}
-
 	return user, nil
 }
