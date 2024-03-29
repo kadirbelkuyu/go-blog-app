1bf7db8f038d39377dc7d6542e40688e30a00f96
mehmet-dogru <mdogru685@gmail.com>
2024-03-29 12:49:45 +0300

    refactor: implemented sql queries to article operations

--- internal/repository/article_repository.go
+++ internal/repository/article_repository.go
@@ -1,10 +1,9 @@
 package repository
 
 import (
+	"database/sql"
 	"errors"
 	"go-blog-app/internal/domain"
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 	"log"
 )
 
@@ -17,19 +16,20 @@ type ArticleRepository interface {
 }
 
 type articleRepository struct {
-	db *gorm.DB
+	db *sql.DB
 }
 
-func NewArticleRepository(db *gorm.DB) ArticleRepository {
+func NewArticleRepository(db *sql.DB) ArticleRepository {
 	return &articleRepository{
 		db: db,
 	}
 }
 
 func (r articleRepository) CreateArticle(article domain.Article) error {
-	result := r.db.Create(&article)
-	if result.Error != nil {
-		log.Printf("create article error %v", result.Error)
+	query := `INSERT INTO articles (title, content, author_id) VALUES ($1, $2, $3)`
+	_, err := r.db.Exec(query, article.Title, article.Content, article.AuthorID)
+	if err != nil {
+		log.Printf("create article error %v", err.Error())
 		return errors.New("failed to create article")
 	}
 	return nil
@@ -37,11 +37,11 @@ func (r articleRepository) CreateArticle(article domain.Article) error {
 
 func (r articleRepository) FindArticleById(id uint) (domain.Article, error) {
 	var article domain.Article
-
-	err := r.db.First(&article, id).Error
+	query := `SELECT * FROM articles WHERE id = $1`
+	err := r.db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Content, &article.AuthorID, &article.CreatedAt, &article.UpdatedAt)
 
 	if err != nil {
-		log.Printf("find article error %v", err)
+		log.Printf("find article by id error %v", err)
 		return domain.Article{}, errors.New("article does not exist")
 	}
 
@@ -49,17 +49,21 @@ func (r articleRepository) FindArticleById(id uint) (domain.Article, error) {
 }
 
 func (r articleRepository) UpdateArticle(id uint, authorId uint, a domain.Article) error {
-	var article domain.Article
-
-	result := r.db.Model(&article).Clauses(clause.Returning{}).Where("id=?", id).Where("author_id=?", authorId).Updates(a)
-
-	if result.Error != nil {
-		log.Printf("error on update %v", result.Error)
+	query := `UPDATE articles SET title = $1, content = $2 WHERE id = $3 AND author_id = $4`
+	result, err := r.db.Exec(query, a.Title, a.Content, id, authorId)
+	if err != nil {
+		log.Printf("error on update %v", err.Error())
 		return errors.New("failed update article")
 	}
 
-	if result.RowsAffected == 0 {
-		return errors.New("article not found or you are not authorized")
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("error on get rows affected %v", err.Error())
+		return errors.New("failed update article")
+	}
+
+	if rowsAffected < 1 {
+		return errors.New("article not found or you are not the author of the article")
 	}
 
 	return nil
@@ -67,26 +71,43 @@ func (r articleRepository) UpdateArticle(id uint, authorId uint, a domain.Articl
 
 func (r articleRepository) GetArticles() ([]domain.Article, error) {
 	var articles []domain.Article
-	err := r.db.Find(&articles).Error
+	query := `SELECT * FROM articles`
+	rows, err := r.db.Query(query)
 	if err != nil {
 		log.Printf("get articles error: %v", err)
 		return nil, errors.New("failed to get articles")
 	}
+
+	for rows.Next() {
+		var article domain.Article
+		err := rows.Scan(&article.ID, &article.Title, &article.Content, &article.AuthorID, &article.CreatedAt, &article.UpdatedAt)
+		if err != nil {
+			log.Printf("error scan article %v", err)
+			return nil, errors.New("failed to get articles")
+		}
+		articles = append(articles, article)
+	}
+
 	return articles, nil
 }
 
 func (r articleRepository) RemoveArticle(id uint, authorId uint) error {
-	var article domain.Article
-	result := r.db.Where("id = ?", id).Where("author_id=?", authorId).Delete(&article)
-
-	if result.Error != nil {
-		log.Printf("error on delete %v", result.Error)
+	query := `DELETE FROM articles WHERE id = $1 AND author_id = $2`
+	result, err := r.db.Exec(query, id, authorId)
+	if err != nil {
+		log.Printf("error on delete %v", err.Error())
 		return errors.New("failed delete to article")
 	}
 
-	if result.RowsAffected == 0 {
-		return errors.New("article not found or you are not authorized")
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("error on get rows affected %v", err.Error())
+		return errors.New("failed delete article")
 	}
 
-	return result.Error
+	if rowsAffected < 1 {
+		return errors.New("article not found or you are not the author of the article")
+	}
+
+	return err
 }
