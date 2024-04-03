4580c9ec778eba7af194f7b9c810e970b23f1441
mehmet-dogru <mdogru685@gmail.com>
2024-04-03 10:16:09 +0300

    fix: removed gorm tags

--- internal/domain/article.go
+++ internal/domain/article.go
@@ -3,10 +3,10 @@ package domain
 import "time"
 
 type Article struct {
-	ID        uint      `json:"id" gorm:"PrimaryKey"`
+	ID        uint      `json:"id"`
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	AuthorID  uint      `json:"author_id"`
-	CreatedAt time.Time `json:"created_at" gorm:"default:current_timestamp"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"default:current_timestamp"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -3,12 +3,12 @@ package domain
 import "time"
 
 type User struct {
-	ID        uint      `json:"id" gorm:"PrimaryKey"`
+	ID        uint      `json:"id"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
-	Email     string    `json:"email" gorm:"index;unique;not null"`
+	Email     string    `json:"email"`
 	Password  string    `json:"password"`
-	Role      string    `json:"role" gorm:"default:author"`
-	CreatedAt time.Time `json:"created_at" gorm:"default:current_timestamp"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"default:current_timestamp"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
